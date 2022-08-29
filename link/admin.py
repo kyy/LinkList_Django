f17ea6983facdb4605f2ce45d846522847ea27f8
kyy <insider_2012@mail.ru>
2022-08-29 10:58:06 +0300

    URL_list model with FK User

--- link/admin.py
+++ link/admin.py
@@ -4,7 +4,7 @@ from .models import URL_list
 
 @admin.register(URL_list)
 class URL_listAdmin(admin.ModelAdmin):
-    list_display = ['URL_short', 'name', 'URL_long', 'data']
+    list_display = ['URL_short', 'user', 'name', 'URL_long', 'data']
     list_editable = ['name', 'URL_long']
     readonly_fields = ['data', 'URL_short']
     ordering = ['data', 'name']
--- link/models.py
+++ link/models.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+from django.contrib.auth.models import User
 from django.db import models
+import datetime
 
 class URL_list(models.Model):
     name = models.CharField(max_length=100)
     URL_long = models.TextField()
     URL_short = models.URLField(max_length=255)
-    data = models.DateTimeField(default=datetime.now())
+    data = models.DateTimeField(default=datetime.datetime.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=3)
 
