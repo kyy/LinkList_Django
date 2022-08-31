dc6af286527a614fbb0aa38f52651624a3908642
kyy <insider_2012@mail.ru>
2022-08-31 16:20:25 +0300

    Ugly checking password

--- registration/forms.py
+++ registration/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
+
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -19,13 +21,31 @@ class MyUserCreationForm(UserCreationForm):
 
 
 class MyAuthenticationForm(AuthenticationForm):
+
     # checking for username  in DB:
     def clean_username(self):
         username = self.cleaned_data['username']
         try:
-            User.objects.get(username=username)
+            User.objects.get(username__exact=username)
         except User.DoesNotExist:
             raise forms.ValidationError(f"The {username} is incorrect username.")
         return username
+
+    # checking password
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        if username and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError('Incorrect password')
+            else:
+                self.confirm_login_allowed(self.user_cache)
+        return self.cleaned_data
+
+
+
+
+
 
 
