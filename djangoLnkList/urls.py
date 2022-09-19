4c398061b84c7faf56bd3ee1b5a44c82e01e44c5
kyy <insider_2012@mail.ru>
2022-09-19 16:01:13 +0300

    full translate + /admin correct use

--- djangoLnkList/urls.py
+++ djangoLnkList/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
-
-urlpatterns = i18n_patterns(
-                            path('', include('registration.urls')),
-                            path('', include('link.urls')),
-                            path('admin/', admin.site.urls),
-                            path('accounts/', include('django.contrib.auth.urls')),
-                            path('i18n/', include('django.conf.urls.i18n')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+urlpatterns += i18n_patterns(
+    path('', include('registration.urls')),
+    path('', include('link.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 )
