aad5e5d69a26e8279603d5d27dd06d5b8cd20442
kyy <insider_2012@mail.ru>
2022-09-21 16:41:13 +0300

    java datatables transl option

--- static/JS/table.js
+++ static/JS/table.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 $('#table_db').DataTable( {
+    // "processing": true,   https://pypi.org/project/django-serverside-datatable/
+    // "serverSide": true,   pip install django-serverside-datatable
     "paging": true,
     "autoWidth": false,
     "searching": true,
@@ -14,5 +16,32 @@ $('#table_db').DataTable( {
         {"targets": 2, "width": "50px", "searchable": false, "orderable": false},
         {"targets": 3, "width": "50x", "searchable": false, "orderable": false },
         {"targets": 4, "width": "220px", "searchable": false, },
-    ]
+    ],
+    "language": {
+        "decimal":        "",
+        "emptyTable":     "No data available in table",
+        "info":           "Showing _START_ to _END_ of _TOTAL_ entries",
+        "infoEmpty":      "Showing 0 to 0 of 0 entries",
+        "infoFiltered":   "(filtered from _MAX_ total entries)",
+        "infoPostFix":    "",
+        "thousands":      ",",
+        "lengthMenu":     "Show _MENU_ entries",
+        "loadingRecords": "Loading...",
+        "processing":     "",
+        "search":         "Search:",
+        "zeroRecords":    "No matching records found",
+        "paginate": {
+            "first":      "First",
+            "last":       "Last",
+            "next":       "Next",
+            "previous":   "Previous"
+        },
+        "aria": {
+            "sortAscending":  ": activate to sort column ascending",
+            "sortDescending": ": activate to sort column descending"
+        }
+    }
+
+
+
 });} );
